9ea76cc8619fa95478e331817095d6d3e3306576
Alex Zotov <sqck@durham.ac.uk>
2025-03-10 16:10:54 +0000

    argument parser + saving results

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import cv2
+import argparse
+import os
+
+def process(img_path):
+    img = cv2.imread(img_path)
+    return img
+
+def main(img_dir):
+    results_dir = "Results"
+    os.makedirs(results_dir, exist_ok=True)
+    for img in os.listdir(img_dir):
+        if img.endswith((".jpg", ".jpeg")):
+            img_path = os.path.join(img_dir, img)
+            processed_img = process(img_path)
+            cv2.imwrite(os.path.join(results_dir, img), processed_img)
+
+parser = argparse.ArgumentParser()
+img_dir = parser.add_argument("img_dir")
+if __name__ == "__main__":
+    args = parser.parse_args()
+    img_dir = args.img_dir
+    main(img_dir)
