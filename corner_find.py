d879c9dfde543c633adbc99532b177dea0610421
Alex Zotov <sqck@durham.ac.uk>
2025-03-11 13:00:50 +0000

    comments

--- corner_find.py
+++ corner_find.py
@@ -1,13 +1,12 @@
 import cv2
 
-def get_mouse_click(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
+def get_mouse_click(event, x, y, flags, param): #prints out clicked point on the image to later manually enter to the dewarping function 
+    if event == cv2.EVENT_LBUTTONDOWN: #on left mouse button click
         print(f"point: ({x}, {y})")
 
-img = cv2.imread("driving_images\im005-snow.jpg")
-cv2.namedWindow("click corners", cv2.WINDOW_NORMAL)
-cv2.setWindowProperty("click corners", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+img = cv2.imread("driving_images\im005-snow.jpg") #read the image (selected an image where the corners are easily distinguishable)
+cv2.namedWindow("click corners", cv2.WINDOW_NORMAL) #create window
+cv2.setWindowProperty("click corners", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN) #make it fullscreen
 cv2.imshow("click corners", img)
 cv2.setMouseCallback("click corners", get_mouse_click)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -3,37 +3,37 @@ import argparse
 import os
 import numpy as np
 
-def dewarp_image_fixed(img):
-    orig_corners = np.float32([
+def dewarp_image(img): #dewarps the image and fixes perspective
+    orig_corners = np.float32([ #coordinates of image corners found by mouse clicks (code: corner_find.py)
         (9, 15),
         (236, 5),
         (30, 244),
         (251, 236)
     ])
-    correct_corners = np.float32([
+    correct_corners = np.float32([ #desired corner coordinates (4 corners of a 256x256 image)
         (0, 0), (256, 0), (0, 256), (256, 256)
     ])
-    matrix = cv2.getPerspectiveTransform(orig_corners, correct_corners)
-    dewarped = cv2.warpPerspective(img, matrix, (256, 256))
+    matrix = cv2.getPerspectiveTransform(orig_corners, correct_corners) #perspective transformation matrix
+    dewarped = cv2.warpPerspective(img, matrix, (256, 256)) #applying matrix
     return dewarped
 
-def process(img_path):
-    img = cv2.imread(img_path)
-    dewarped = dewarp_image_fixed(img)  # Apply dewarping
+def process(img_path): #applies all the processing function to the image
+    img = cv2.imread(img_path) #read the image
+    dewarped = dewarp_image(img)
     return dewarped
 
 def main(img_dir):
     results_dir = "Results"
-    os.makedirs(results_dir, exist_ok=True)
-    for img in os.listdir(img_dir):
-        if img.endswith((".jpg", ".jpeg")):
-            img_path = os.path.join(img_dir, img)
-            processed_img = process(img_path)
-            cv2.imwrite(os.path.join(results_dir, img), processed_img)
+    os.makedirs(results_dir, exist_ok=True) #create the results directory if doesn not exist
+    for img in os.listdir(img_dir): #loop through images in the given directory
+        if img.endswith((".jpg", ".jpeg")): #make sure to skip any non image files such as .DS_Store
+            img_path = os.path.join(img_dir, img) #get image path
+            processed_img = process(img_path) 
+            cv2.imwrite(os.path.join(results_dir, img), processed_img) #write processed image into results directory
 
 parser = argparse.ArgumentParser()
-img_dir = parser.add_argument("img_dir")
+img_dir = parser.add_argument("img_dir") #create image directory argument
 if __name__ == "__main__":
-    args = parser.parse_args()
-    img_dir = args.img_dir
+    args = parser.parse_args() #parse argumets
+    img_dir = args.img_dir 
     main(img_dir)
