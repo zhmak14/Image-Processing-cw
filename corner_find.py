aed34b1c655d86242d6e9aab3b0476fc39199324
Alex Zotov <sqck@durham.ac.uk>
2025-03-21 23:50:04 +0000

    pep8 satisfied

--- corner_find.py
+++ corner_find.py
@@ -1,12 +1,21 @@
 import cv2
 
-def get_mouse_click(event, x, y, flags, param): #prints out clicked point on the image to later manually enter to the dewarping function 
-    if event == cv2.EVENT_LBUTTONDOWN: #on left mouse button click
+
+# prints out clicked point on the image
+def get_mouse_click(event, x, y, flags, param):
+    # on left mouse button click
+    if event == cv2.EVENT_LBUTTONDOWN:
         print(f"point: ({x}, {y})")
 
-img = cv2.imread("driving_images\im005-snow.jpg") #read the image (selected an image where the corners are easily distinguishable)
-cv2.namedWindow("click corners", cv2.WINDOW_NORMAL) #create window
-cv2.setWindowProperty("click corners", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN) #make it fullscreen
+
+# read the image (selected one where the corners are easily visible)
+img = cv2.imread("driving_images/im005-snow.jpg")
+# create window
+cv2.namedWindow("click corners", cv2.WINDOW_NORMAL)
+# make it fullscreen
+cv2.setWindowProperty(
+    "click corners", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN
+)
 cv2.imshow("click corners", img)
 cv2.setMouseCallback("click corners", get_mouse_click)
 cv2.waitKey(0)
--- main.py
+++ main.py
@@ -3,66 +3,105 @@ import argparse
 import os
 import numpy as np
 
-def dewarp_image(img): #dewarps the image and fixes perspective
-    orig_corners = np.float32([ #coordinates of image corners found by mouse clicks (code: corner_find.py)
-        (9, 15), 
+
+# dewarps the image fixing perspective
+def dewarp_image(img):
+    # coordinates of image corners found by mouse clicks (code: corner_find.py)
+    orig_corners = np.float32([
+        (9, 15),
         (236, 5),
         (30, 244),
         (251, 236)
     ])
-    correct_corners = np.float32([ #desired corner coordinates (4 corners of a 256x256 image)
+    # desired corner coordinates (4 corners of a 256x256 image)
+    correct_corners = np.float32([
         (0, 0), (256, 0), (0, 256), (256, 256)
     ])
-    matrix = cv2.getPerspectiveTransform(orig_corners, correct_corners) #perspective transformation matrix
-    dewarped = cv2.warpPerspective(img, matrix, (256, 256)) #applying matrix
+    # perspective transformation matrix
+    matrix = cv2.getPerspectiveTransform(orig_corners, correct_corners)
+    # applying matrix
+    dewarped = cv2.warpPerspective(img, matrix, (256, 256))
     return dewarped
 
-def inpaint_missing(img): #fills the missing region on the image
-    upper_right = img[0:256//2, 256//2:256] #crops the image to only top right fourth, knowing that the missing region can only appear there
+
+# fills the missing region on the image
+def inpaint_missing(img):
+    # crops the image to only top right fourth where missing region always is
+    upper_right = img[0:256//2, 256//2:256]
     grayscale = cv2.cvtColor(upper_right, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(grayscale, 7, 255, cv2.THRESH_BINARY) #binary threshholding on a grayscaled image to prepare for edge detection
+    # binary threshholding on a grayscaled image to prepare for edge detection
+    ret, thresh = cv2.threshold(grayscale, 7, 255, cv2.THRESH_BINARY)
+    # canny edge detection
     edge = cv2.Canny(thresh, 100, 150)
-    contours, hierarchy = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #find the contours on the Canny applied image
-    max_contour = max(contours, key=cv2.contourArea) #select max contour by area
-    (x, y), radius = cv2.minEnclosingCircle(max_contour) #create a circle of radius and with center coordinates of the missing region contour
-    mask = np.zeros(img.shape[:2], np.uint8) #create an all black mask of the same size as the image
-    cv2.circle(mask, (int(x + 256//2), int(y)), int(radius + 2), (255), -1) #draw a the white circle of the missing region properties on the mask 
-    inpainted = cv2.inpaint(img, mask, 20, cv2.INPAINT_TELEA) #apply mask to the image and inpaint the circle
+    # find the contours on the Canny applied image
+    contours, hierarchy = cv2.findContours(
+        edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )
+    # select max contour by area
+    max_contour = max(contours, key=cv2.contourArea)
+    # create a circle of radius and coordinates of the missing region
+    (x, y), radius = cv2.minEnclosingCircle(max_contour)
+    # create an all black mask of the same size as the image
+    mask = np.zeros(img.shape[:2], np.uint8)
+    # draw a the white circle of the missing region properties on the mask
+    cv2.circle(mask, (int(x + 256//2), int(y)), int(radius + 2), (255), -1)
+    # apply mask to the image and inpaint the circle
+    inpainted = cv2.inpaint(img, mask, 20, cv2.INPAINT_TELEA)
     return inpainted
 
+
+# denoises the image
 def remove_noise(img):
-    denoised = cv2.bilateralFilter(img, 15, 75, 75) #bilateral filter to remove whatever noise we can before median blur
+    # bilateral filter to remove whatever noise we can before median blur
+    denoised = cv2.bilateralFilter(img, 15, 75, 75)
     b, g, r = cv2.split(denoised)
-    b_blurred = cv2.medianBlur(b, 3) #median blur only to the blue and green channels as they introduce most of the remaining noise
+    # median blur to the blue and green channels as they introduce most noise
+    b_blurred = cv2.medianBlur(b, 3)
     g_blurred = cv2.medianBlur(g, 3)
     denoised = cv2.merge([b_blurred, g_blurred, r])
-    denoised = cv2.medianBlur(denoised, 3) #median blur again on the entire image to further smoothen the rest of salt/pepper leaving just colour spots
-    denoised = cv2.fastNlMeansDenoisingColored(denoised, None, 1, 9, 7, 20) #Non-local means coloured to remove colour spots without denoising the rest further
+    # median blur again on the entire image to further smoothen the noise
+    denoised = cv2.medianBlur(denoised, 3)
+    # Non-local means coloured to remove colour spots
+    denoised = cv2.fastNlMeansDenoisingColored(denoised, None, 1, 9, 7, 20)
     return denoised
 
-def balance_colours(img): #balances colours according to gray world assumtion
+
+# balances colours according to gray world assumtion
+def balance_colours(img):
     b, g, r = cv2.split(img)
+    # calculate mean of all pixel brightnesses across 3 channels
     b_mean = np.mean(b)
     g_mean = np.mean(g)
     r_mean = np.mean(r)
-    mean_all = (b_mean + g_mean + r_mean) / 3 #mean of all pixel brightnesses across 3 channels
+    mean_all = (b_mean + g_mean + r_mean) / 3
+    # scale each channel to have equal brightness
     b = np.clip(b * (mean_all / b_mean), 0, 255).astype(np.uint8)
-    g = np.clip(g * (mean_all / g_mean), 0, 255).astype(np.uint8) #scale each channel to have equal brightness
+    g = np.clip(g * (mean_all / g_mean), 0, 255).astype(np.uint8)
     r = np.clip(r * (mean_all / r_mean), 0, 255).astype(np.uint8)
     balanced = cv2.merge([b, g, r])
     return balanced
 
+
+# adjusts contrast and brightness of the image
 def fix_contrast_brightness(img):
-    adjusted = cv2.convertScaleAbs(img, None, 0.97, 1) #very slightly lowers the contrast and brightens the image
+    # very slightly lowers the contrast and brightens the image
+    adjusted = cv2.convertScaleAbs(img, None, 0.97, 1)
     return adjusted
 
-def sharpen_image(img): #unsharp mask sharpening method
-    blur_mask = cv2.GaussianBlur(img, (0, 0), 1.5) #Gaussian blur to soften the image creating the mask
-    sharpened = cv2.addWeighted(img, 1 + 1.5, blur_mask, -1.5, 0) #sharpens the image by subtracting blurred mask from the original
+
+# unsharp mask sharpening method
+def sharpen_image(img):
+    # Gaussian blur to soften the image creating the mask
+    blur_mask = cv2.GaussianBlur(img, (0, 0), 1.5)
+    # sharpens the image by subtracting blurred mask from the original
+    sharpened = cv2.addWeighted(img, 1 + 1.5, blur_mask, -1.5, 0)
     return sharpened
 
-def process(img_path): #applies all the processing function to the image
-    img = cv2.imread(img_path) #read the image
+
+# processing pipeline, applies every function to the image
+def process(img_path):
+    # read the image
+    img = cv2.imread(img_path)
     dewarped = dewarp_image(img)
     inpainted = inpaint_missing(dewarped)
     denoised = remove_noise(inpainted)
@@ -71,18 +110,30 @@ def process(img_path): #applies all the processing function to the image
     sharpened = sharpen_image(contrasted)
     return sharpened
 
-def main(img_dir):
-    results_dir = "Results"
-    os.makedirs(results_dir, exist_ok=True) #create the results directory if doesn not exist
-    for img in os.listdir(img_dir): #loop through images in the given directory
-        if img.endswith((".jpg", ".jpeg")): #make sure to skip any non image files such as .DS_Store
-            img_path = os.path.join(img_dir, img) #get image path
-            processed_img = process(img_path) 
-            cv2.imwrite(os.path.join(results_dir, img), processed_img) #write processed image into results directory
 
+# creates the results directory and writes the processsed images
+def main(img_dir):
+    # create the results directory if doesn not exist
+    results_dir = "Results"
+    os.makedirs(results_dir, exist_ok=True)
+    # loop through images in the given directory
+    for img in os.listdir(img_dir):
+        # make sure to skip any non image files such as .DS_Store
+        if img.endswith((".jpg", ".jpeg")):
+            # get image path
+            img_path = os.path.join(img_dir, img)
+            processed_img = process(img_path)
+            # write processed image into results directory
+            cv2.imwrite(os.path.join(results_dir, img), processed_img)
+
+
+# argumet parser to let user specify the images' directory
 parser = argparse.ArgumentParser()
-img_dir = parser.add_argument("img_dir") #create image directory argument
+# create image directory argument
+img_dir = parser.add_argument("img_dir")
 if __name__ == "__main__":
-    args = parser.parse_args() #parse argumets
-    img_dir = args.img_dir 
+    # parse argumets
+    args = parser.parse_args()
+    img_dir = args.img_dir
+    # call main function on the input directory
     main(img_dir)
